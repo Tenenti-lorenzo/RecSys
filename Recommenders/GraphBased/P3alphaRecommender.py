2aeb1322c0f776b0e7b4552c34db8eeb741876b5
Tenenti-lorenzo <65445129+Tenenti-lorenzo@users.noreply.github.com>
2023-01-04 00:18:03 +0100

    r

--- Recommenders/GraphBased/P3alphaRecommender.py
+++ Recommenders/GraphBased/P3alphaRecommender.py
@@ -128,123 +128,33 @@ class P3alphaRecommender(BaseItemSimilarityMatrixRecommender):
         self.W_sparse = check_matrix(self.W_sparse, format='csr')
 
 
-class P3alphaRecommender_stacked(BaseItemSimilarityMatrixRecommender):
-    """ P3alpha recommender """
+class P3alphaRecommenderICM(P3alphaRecommender):
+    """ RP3beta recommender ICM based """
 
-    RECOMMENDER_NAME = "P3alphaRecommender"
+    RECOMMENDER_NAME = "P3alphaRecommenderICM"
 
-    def __init__(self, URM_train, ICM_TYPE,ICM_LENGHT, verbose = True):
-        super(P3alphaRecommender_stacked, self).__init__(self, URM_train, ICM_TYPE,ICM_LENGHT, verbose = verbose)
+    def __init__(self, URM_train, ICM_LENGHT, ICM_TYPE, verbose = True):
+        super(P3alphaRecommenderICM, self).__init__(URM_train, verbose = verbose)
         self.ICM_type = check_matrix(ICM_TYPE.copy().T, 'csr', dtype=np.float32)
         self.ICM_type.eliminate_zeros()
         self.ICM_lenght = check_matrix(ICM_LENGHT.copy().T, 'csr', dtype=np.float32)
         self.ICM_lenght.eliminate_zeros()
         self.URM_original = self.URM_train
 
+
     def __str__(self):
-        return "P3alpha(alpha={}, min_rating={}, topk={}, implicit={}, normalize_similarity={})".format(self.alpha,
-                                                                            self.min_rating, self.topK, self.implicit,
-                                                                            self.normalize_similarity)
+        return "RP3alpha(alpha={},  min_rating={}, topk={}, implicit={}, normalize_similarity={})".format(self.alpha,
+                                                                                         self.min_rating, self.topK,
+                                                                                        self.implicit, self.normalize_similarity)
 
-    def fit(self, topK=100, alpha=1., min_rating=0, implicit=False,mw = 0,mw_1=0, normalize_similarity=False):
-
-        self.topK = topK
-        self.alpha = alpha
-        self.min_rating = min_rating
-        self.implicit = implicit
-        self.normalize_similarity = normalize_similarity
-
+    def fit(self,mw_1,mw, **fit_args):
         self.mw = mw
         self.mw_1 = mw_1
 
         self.URM_train = self.URM_original
         for i in range(0,mw):
-            self.URM_train = sps.vstack((self.URM_train, self.ICM_type))
+            self.URM_train = sps.vstack((self.URM_train, self.ICM_type.T))
         for i in range(0,mw_1):
-            self.URM_train = sps.vstack((self.URM_train, self.ICM_lenght))
-
-        #
-        # if X.dtype != np.float32:
-        #     print("P3ALPHA fit: For memory usage reasons, we suggest to use np.float32 as dtype for the dataset")
-
-        if self.min_rating > 0:
-            self.URM_train.data[self.URM_train.data < self.min_rating] = 0
-            self.URM_train.eliminate_zeros()
-            if self.implicit:
-                self.URM_train.data = np.ones(self.URM_train.data.size, dtype=np.float32)
-
-        #Pui is the row-normalized urm
-        Pui = normalize(self.URM_train, norm='l1', axis=1)
-
-        #Piu is the column-normalized, "boolean" urm transposed
-        X_bool = self.URM_train.transpose(copy=True)
-        X_bool.data = np.ones(X_bool.data.size, np.float32)
-        #ATTENTION: axis is still 1 because i transposed before the normalization
-        Piu = normalize(X_bool, norm='l1', axis=1)
-        del(X_bool)
-
-        # Alfa power
-        if self.alpha != 1.:
-            Pui = Pui.power(self.alpha)
-            Piu = Piu.power(self.alpha)
-
-        # Final matrix is computed as Pui * Piu * Pui
-        # Multiplication unpacked for memory usage reasons
-        block_dim = 200
-        d_t = Piu
-
-        similarity_builder = Incremental_Similarity_Builder(Pui.shape[1], initial_data_block=Pui.shape[1]*self.topK, dtype = np.float32)
-
-        start_time = time.time()
-        start_time_printBatch = start_time
-
-        for current_block_start_row in range(0, Pui.shape[1], block_dim):
-
-            if current_block_start_row + block_dim > Pui.shape[1]:
-                block_dim = Pui.shape[1] - current_block_start_row
-
-            similarity_block = d_t[current_block_start_row:current_block_start_row + block_dim, :] * Pui
-            similarity_block = similarity_block.toarray()
-
-            for row_in_block in range(block_dim):
-                row_data = similarity_block[row_in_block, :]
-                row_data[current_block_start_row + row_in_block] = 0
-
-                best = row_data.argsort()[::-1][:self.topK]
-
-                notZerosMask = row_data[best] != 0.0
-
-                values_to_add = row_data[best][notZerosMask]
-                cols_to_add = best[notZerosMask]
-
-                similarity_builder.add_data_lists(row_list_to_add=np.ones(len(values_to_add)) * (current_block_start_row + row_in_block),
-                                                  col_list_to_add=cols_to_add,
-                                                  data_list_to_add=values_to_add)
-
-
-            if time.time() - start_time_printBatch > 300:
-                new_time_value, new_time_unit = seconds_to_biggest_unit(time.time() - start_time)
-
-                self._print("Similarity column {} ({:4.1f}%), {:.2f} column/sec. Elapsed time {:.2f} {}".format(
-                     current_block_start_row + block_dim,
-                    100.0 * float( current_block_start_row + block_dim) / Pui.shape[1],
-                    float( current_block_start_row + block_dim) / (time.time() - start_time),
-                    new_time_value, new_time_unit))
-
-                sys.stdout.flush()
-                sys.stderr.flush()
-
-                start_time_printBatch = time.time()
-
-
-        self.W_sparse = similarity_builder.get_SparseMatrix()
-
-
-        if self.normalize_similarity:
-            self.W_sparse = normalize(self.W_sparse, norm='l1', axis=1)
-
-
-        if self.topK != False:
-            self.W_sparse = similarityMatrixTopK(self.W_sparse, k=self.topK)
-
-        self.W_sparse = check_matrix(self.W_sparse, format='csr')
+            self.URM_train = sps.vstack((self.URM_train, self.ICM_lenght.T))
+            
+        super(P3alphaRecommenderICM, self).fit(**fit_args)
