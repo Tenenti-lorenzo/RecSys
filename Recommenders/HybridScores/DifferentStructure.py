b4ab4435e24d40e4f33908ef0274d8d6af91b62e
Tenenti-lorenzo <65445129+Tenenti-lorenzo@users.noreply.github.com>
2023-01-05 22:38:19 +0100

    three

--- Recommenders/HybridScores/DifferentStructure.py
+++ Recommenders/HybridScores/DifferentStructure.py
@@ -59,7 +59,7 @@ class TwoDifferentModelRecommender(BaseRecommender):
 
         return item_weights
 
-class ThreeDifferentModelRecommender(BaseRecommender):
+class ThreeDifferentModelRecommender1(BaseRecommender):
     '''
     Hybrid of 3 predictions
     '''
@@ -77,7 +77,7 @@ class ThreeDifferentModelRecommender(BaseRecommender):
 
 
     def __init__(self, URM_train, recommender_1, recommender_2, recommender_3):
-        super(ThreeDifferentModelRecommender, self).__init__(URM_train)
+        super(ThreeDifferentModelRecommender1, self).__init__(URM_train)
 
         self.URM_train = sps.csr_matrix(URM_train)
         self.recommender_1 = recommender_1
@@ -115,6 +115,75 @@ class ThreeDifferentModelRecommender(BaseRecommender):
         norm_item_weights_1 = LA.norm(item_weights_1, self.norm2) if self.norm is 2 and self.norm is 1 else LA.norm(item_weights_1, self.norm)
         norm_item_weights_2 = LA.norm(item_weights_2, self.norm2) if self.norm is 2 and self.norm is 1 else LA.norm(item_weights_2, self.norm)
         norm_item_weights_3 = LA.norm(item_weights_3, self.norm2) if self.norm is 2 and self.norm is 1 else LA.norm(item_weights_3, self.norm)
+        
+        
+        if norm_item_weights_1 == 0:
+            raise ValueError("Norm {} of item weights for recommender 1 is zero. Avoiding division by zero".format(self.norm))
+        
+        if norm_item_weights_2 == 0:
+            raise ValueError("Norm {} of item weights for recommender 2 is zero. Avoiding division by zero".format(self.norm))
+            
+        if norm_item_weights_3 == 0:
+            raise ValueError("Norm {} of item weights for recommender 3 is zero. Avoiding division by zero".format(self.norm))
+        
+        item_weights = item_weights_1 / norm_item_weights_1 * self.alpha + item_weights_2 / norm_item_weights_2 * self.beta + item_weights_3 / norm_item_weights_3 * self.gamma
+
+        return item_weights
+
+    def save_model(self, folder_path, file_name = None):
+        print("Not saving")
+
+class ThreeDifferentModelRecommender(BaseRecommender):
+    '''
+    Hybrid of 3 predictions
+    '''
+
+    '''
+    hyperparameters_range_dictionary = {
+        "norm" : Categorical([1, 2, np.inf, -np.inf]),
+        "alpha" :Real(low = 0, high = 1, prior = 'uniform'),
+        "beta" :Real(low = 0, high = 1, prior = 'uniform'),
+        "gamma" :Real(low = 0, high = 1, prior = 'uniform')
+    }
+    '''
+
+    RECOMMENDER_NAME = "DifferentLossScoresHybridRecommender"
+
+
+    def __init__(self, URM_train, recommender_1, recommender_2, recommender_3):
+        super(ThreeDifferentModelRecommender, self).__init__(URM_train)
+
+        self.URM_train = sps.csr_matrix(URM_train)
+        self.recommender_1 = recommender_1
+        self.recommender_2 = recommender_2
+        self.recommender_3 = recommender_3
+        
+        
+        
+    def fit(self, norm, alpha = 0.5, beta = 0.5, gamma = 0.5):
+
+        sump = alpha+beta+gamma
+
+        self.alpha = alpha/sump
+        self.beta = beta/sump
+        self.gamma = gamma/sump
+        self.norm = norm
+
+        print(f"CURRENT CONFIGURATION:\n{self.recommender_1.RECOMMENDER_NAME} with weight alpha: {self.alpha}")
+        print(f"{self.recommender_2.RECOMMENDER_NAME} with weight beta: {self.beta}")
+        print(f"{self.recommender_3.RECOMMENDER_NAME} with weight gamma: {self.gamma}")
+        print(f"Norm type: {self.norm}")
+
+
+    def _compute_item_score(self, user_id_array, items_to_compute):
+        
+        item_weights_1 = self.recommender_1._compute_item_score(user_id_array)
+        item_weights_2 = self.recommender_2._compute_item_score(user_id_array)
+        item_weights_3 = self.recommender_3._compute_item_score(user_id_array)
+
+        norm_item_weights_1 = 1 if self.norm is None else LA.norm(item_weights_1, self.norm)
+        norm_item_weights_2 = 1 if self.norm is None else LA.norm(item_weights_2, self.norm)
+        norm_item_weights_3 = 1 if self.norm is None else LA.norm(item_weights_3, self.norm)
         
         
         if norm_item_weights_1 == 0:
