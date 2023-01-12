0bfc55e6c68c0701fb1e685a06044e648f361fb4
Manuelcp <manuel.cecere@stud.unifi.it>
2023-01-12 18:49:14 +0100

    varie

--- Recommenders/HybridScores/DifferentStructure.py
+++ Recommenders/HybridScores/DifferentStructure.py
@@ -52,6 +52,55 @@ class TwoDifferentModelRecommender12(BaseRecommender):
 
         return item_weights
 
+class TwoDifferentModelRecommender(BaseRecommender):
+    '''
+    Hybrid of 2 predictions
+    '''
+
+    '''
+    hyperparameters_range_dictionary = {
+        "norm" : Categorical([1, 2, np.inf, -np.inf]),
+        "alpha" :Real(low = 0.0001, high = 0.9999, prior = 'uniform')
+    }
+    '''
+
+    RECOMMENDER_NAME = "DifferentLossScoresHybridRecommender"
+
+
+    def __init__(self, URM_train, recommender_1, recommender_2):
+        super(TwoDifferentModelRecommender12, self).__init__(URM_train)
+
+        self.URM_train = sps.csr_matrix(URM_train)
+        self.recommender_1 = recommender_1
+        self.recommender_2 = recommender_2
+        
+        
+        
+    def fit(self, norm = None, alpha = 0.5):
+
+        self.alpha = alpha
+        self.norm = norm
+
+
+    def _compute_item_score(self, user_id_array,items_to_compute = None):
+        
+        item_weights_1 = self.recommender_1._compute_item_score(user_id_array)
+        item_weights_2 = self.recommender_2._compute_item_score(user_id_array)
+
+        norm_item_weights_1 = 1 if self.norm is None else LA.norm(item_weights_1, self.norm)
+        norm_item_weights_2 = 1 if self.norm is None else LA.norm(item_weights_2, self.norm)
+        
+        
+        if norm_item_weights_1 == 0:
+            raise ValueError("Norm {} of item weights for recommender 1 is zero. Avoiding division by zero".format(self.norm))
+        
+        if norm_item_weights_2 == 0:
+            raise ValueError("Norm {} of item weights for recommender 2 is zero. Avoiding division by zero".format(self.norm))
+        
+        item_weights = item_weights_1 / norm_item_weights_1 * self.alpha + item_weights_2 / norm_item_weights_2 * (1-self.alpha)
+
+        return item_weights
+
 class ThreeDifferentModelRecommender1(BaseRecommender):
     '''
     Hybrid of 3 predictions
